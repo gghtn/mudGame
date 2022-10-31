984eb5fc495719cc59ea57ba9ff61cff19e3a02e
gghtn <ghtn9898@gmail.com>
2022-10-31 22:58:32 +0900

    user class

--- mudGame/user.cpp
+++ mudGame/user.cpp
@@ -1,44 +1,355 @@
 #include "user.h"
+#include "skill.h"
+#include "key_pos.h"
 #include <stdlib.h>
 #include <time.h>
+#include <conio.h>
 
-CUser::CUser(string name) :CObject() { 
-	mb_normalAtk = true, mb_skillAtk = false;
-	mWeaponATK = 100, mPoint = 0, mExp = 0, mTotalExp = 100, mGold = 0;
-	skill = 0;
-	
+int CUser::killCount = 0;
+
+CUser::CUser(string name) :CObject() {
+	mWeaponClass = 1, mWeaponATK = 100, mAccuracy = 10, mResilience = 10, mPoint = 0, mExp = 0, mTotalExp = 100, mGold = 0, mItem = 0;
+	for (int i = 0; i < 4; i++) {
+		skill[i] = false;
+	}
 	SetInitAll(name);
 }
 
-string CUser::Attack(CObject* pObject) {
-	char result[100];
-	int damage, avoid;
+//일반 공격
+string CUser::Attack(CObject* pMonster) {
+	char result[300];
+	int damage;
+	bool attack_failed;
 	float weight;
 
-	weight = (float)GetLevel() - pObject->GetLevel();
-	if (weight < -20 && weight > -100) weight = 1.0f + (weight / 100);
-	else if (weight < -100) weight = 0.0f;
-	else if (weight > 20) weight = 1.0f + (weight / 100);
-	else weight = 1.0f;
+	attack_failed = AttackFail(pMonster);
 
-	avoid = pObject->GetDEX() + pObject->GetLevel() - GetLevel();
-	if (avoid <= 0) avoid = 0;
+	if (attack_failed) damage = 0;
 	else {
-		if (rand() % 10 < avoid / 10) avoid = 1;
-		else avoid = 0;
+		weight = LevelWeight(pMonster);
+		damage = int((((rand() % 5 + 50) * GetSTR() + mWeaponATK) / pMonster->GetDEF()) * weight);
 	}
 
-	if (avoid) damage = 0;
-	else {
-		damage = int((((rand()%20 + 80) * GetSTR() + mWeaponATK) / pObject->GetDEF()) * weight);
-	}
-
-	pObject->SetDamage(damage);
+	pMonster->SetDamage(damage);
 	if (damage == 0)
-		sprintf(result, "%s이 공격이 빗나갔습니다!", GetName());
-	else if(pObject->GetLive())
-		sprintf(result, "%s의 공격으로 %s에게 %d만큼의 피해를 입혔습니다!", GetName(), pObject->GetName(), damage);
-	else sprintf(result, "%s의 공격으로 %s이(가) %d의 피해를 입고 죽었습니다!", GetName(), pObject->GetName(), damage);
+		sprintf(result, "공격이 빗나갔습니다!");
+	else if (pMonster->GetLive())
+		sprintf(result, "%s에게 %d만큼의 피해를 입혔습니다! [HP: %d / %d]",
+			pMonster->GetName(), damage, pMonster->GetHP(), pMonster->GetMaxHP());
+	else {
+		sprintf(result, "%s의 공격으로 %s이(가) %d의 피해를 입고 죽었습니다!", GetName(), pMonster->GetName(), damage);
+		killCount++;
+		strcat(result, Loot(pMonster));
+	}
 
 	return result;
+}
+
+//스킬 선택
+string CUser::SkillActivate(CObject* pMonster) {
+	int key, mp;
+
+	while (1) {
+		key = getch();
+
+		switch (key) {
+		case ESC:
+			return "0";
+
+		case A_KEY:
+			if (skill[0]) {
+				if (GetMP() >= TRIPLE_ATTACK_MP)
+					return TripleAttack(pMonster, TRIPLE_ATTACK_MP);
+				else
+					continue;
+			}
+			else {
+				continue;
+			}
+			break;
+
+		case S_KEY:
+			if (skill[1]) {
+				mp = (GetMaxMP() / 10) * RECOVERY_MP;
+				if (GetMP() >= mp)
+					return Recovery(mp);
+				else
+					continue;
+			}
+			else {
+				continue;
+			}
+			break;
+
+		case D_KEY:
+			if (skill[2]) {
+				mp = (GetMaxMP() / 10) * TRUE_ATTACK_MP;
+				if (GetMP() >= mp)
+					return TrueAttack(pMonster, mp);
+				else
+					continue;
+			}
+			else {
+				continue;
+			}
+			break;
+
+		case F_KEY:
+			if (skill[3]) {
+				mp = (GetMaxMP() / 10) * AURA_BLADE_MP;
+				if (GetMP() >= mp)
+					return AuraBlade(pMonster, mp);
+				else
+					continue;
+			}
+			else {
+				continue;
+			}
+			break;
+
+		default:
+			continue;
+		}
+	}
+
+}
+
+//공격 성공여부
+bool CUser::AttackFail(CObject* pMonster) {
+	bool attack_failed;
+	int avoid;
+	avoid = pMonster->GetDEX() + pMonster->GetLevel() - GetLevel() - mAccuracy;
+	if (avoid <= 0) attack_failed = false;
+	else {
+		if (rand() % 10 < avoid / 10) attack_failed = true;
+		else attack_failed = 0;
+	}
+	return attack_failed;
+}
+
+//스킬
+string CUser::TripleAttack(CObject* pMonster, int mp) {
+	char result[300] = "[스킬] 트리플 어택\n", tmp[100];
+	int damage;
+	bool attack_failed;
+	float weight;
+
+	UseMP(mp);		//MP소모
+
+	for (int i = 0; i < 3; i++) {
+		attack_failed = AttackFail(pMonster);
+		if (attack_failed) damage = 0;
+		else {
+			weight = LevelWeight(pMonster);
+			damage = int((((rand() % 5 + 50) * (GetSTR() + TRIPLE_ATTACK) + mWeaponATK) / pMonster->GetDEF()) * weight);
+		}
+
+		pMonster->SetDamage(damage);
+		if (damage == 0)
+			switch (i) {
+			case 0:
+				sprintf(tmp, " [[일격]] 빗나갔습니다!");
+				break;
+			case 1:
+				sprintf(tmp, " [[이격]] 빗나갔습니다!");
+				break;
+			case 2:
+				sprintf(tmp, " [[삼격]] 빗나갔습니다!");
+				break;
+			}
+		else if (pMonster->GetLive())
+			switch (i) {
+			case 0:
+				sprintf(tmp, " [[일격]] %s에게 %d만큼의 피해를 입혔습니다! [HP: %d / %d]",
+					pMonster->GetName(), damage, pMonster->GetHP(), pMonster->GetMaxHP());
+				break;
+			case 1:
+				sprintf(tmp, " [[이격]] %s에게 %d만큼의 피해를 입혔습니다! [HP: %d / %d]",
+					pMonster->GetName(), damage, pMonster->GetHP(), pMonster->GetMaxHP());
+				break;
+			case 2:
+				sprintf(tmp, " [[삼격]] %s에게 %d만큼의 피해를 입혔습니다! [HP: %d / %d]",
+					pMonster->GetName(), damage, pMonster->GetHP(), pMonster->GetMaxHP());
+				break;
+			}
+
+		else {
+			switch (i) {
+			case 0:
+				sprintf(tmp, " [[일격]] %s이(가) %d의 피해를 입고 죽었습니다!", pMonster->GetName(), damage);
+				killCount++;
+				break;
+			case 1:
+				sprintf(tmp, " [[이격]] %s이(가) %d의 피해를 입고 죽었습니다!", pMonster->GetName(), damage);
+				killCount++;
+				break;
+			case 2:
+				sprintf(tmp, " [[삼격]] %s이(가) %d의 피해를 입고 죽었습니다!", pMonster->GetName(), damage);
+				killCount++;
+				break;
+			}
+			strcat(result, tmp);
+			strcat(result, Loot(pMonster));
+			break;
+		}
+
+		strcat(result, tmp);
+		strcat(result, "\n");
+	}
+	return result;
+}
+string CUser::Recovery(int mp) {
+	char result[100] = "[스킬] 회복\n", tmp[100];
+	int recovery, reHP;
+
+	UseMP(mp);
+
+	recovery = (GetMaxHP() / 10) * RECOVERY;	//3할 회복
+	if (GetHP() + recovery >= GetMaxHP()) reHP = GetMaxHP();
+	else reHP = GetHP() + recovery;
+
+	SetHP(reHP);
+
+	sprintf(tmp, " 회복의 기운으로 %d의 생명을 회복합니다!", recovery);
+	strcat(result, tmp);
+
+	return result;
+}
+string CUser::TrueAttack(CObject* pMonster, int mp) {
+	char result[300] = "[스킬] 트루 어택\n", tmp[100];
+	int damage;
+	bool attack_failed;
+	float weight;
+
+	UseMP(mp);
+
+	attack_failed = AttackFail(pMonster);
+	if (attack_failed) damage = 0;
+	else {
+		weight = LevelWeight(pMonster);
+		damage = int((((rand() % 5 + 30) * (GetSTR() + TRUE_ATTACK) + mWeaponATK)) * weight);
+	}
+
+	pMonster->SetDamage(damage);
+	if (damage == 0)
+		sprintf(tmp, " 공격이 빗나갔습니다!");
+	else if (pMonster->GetLive())
+		sprintf(tmp, " 방어력 무시 치명타!!\n %s에게 %d만큼의 피해를 입혔습니다! [HP: %d / %d]",
+			pMonster->GetName(), damage, pMonster->GetHP(), pMonster->GetMaxHP());
+	else {
+		sprintf(tmp, " 치명적인 일격!!\n %s이(가) 즉사했습니다!!", pMonster->GetName());
+		killCount++;
+		strcat(result, tmp);
+		strcat(result, Loot(pMonster));
+		return result;
+	}
+	strcat(result, tmp);
+	return result;
+}
+string CUser::AuraBlade(CObject* pMonster, int mp) {
+	char result[300] = "[스킬] 오러 블레이드\n", tmp[100];
+	int damage;
+	bool attack_failed;
+	float weight;
+
+	UseMP(mp);
+
+	attack_failed = AttackFail(pMonster);
+	if (attack_failed) damage = 0;
+	else {
+		weight = LevelWeight(pMonster);
+		damage = int((((rand() % 5 + 50) * (GetSTR() + AURA_BLADE) + mWeaponATK) / pMonster->GetDEF()) * weight);
+	}
+
+	pMonster->SetDamage(damage);
+	if (damage == 0)
+		sprintf(tmp, " 공격이 빗나갔습니다!");
+	else if (pMonster->GetLive())
+		sprintf(tmp, " 오러 블레이드로 %s에게 %d만큼의 피해를 입혔습니다! [HP: %d / %d]",
+			pMonster->GetName(), damage, pMonster->GetHP(), pMonster->GetMaxHP());
+	else {
+		sprintf(tmp, " 치명적인 일격!!\n %s이(가) 즉사했습니다!!", pMonster->GetName());
+		killCount++;
+		strcat(result, tmp);
+		strcat(result, Loot(pMonster));
+		return result;
+	}
+	strcat(result, tmp);
+	return result;
+}
+
+char* CUser::Loot(CObject* pMonster) {
+	char result[100];
+	int gold;
+	CMonster* pM = dynamic_cast<CMonster*>(pMonster);
+	mExp += pM->GetMonsterExp();
+	while (!(mExp < mTotalExp)) {
+		LevelUp();
+	}
+	gold = pM->GetMonsterGold();
+	mGold += gold;
+	if (rand() % 100 < pM->GetItemWeight()) {
+		mItem++;
+		sprintf(result, "\n [%d골드 획득!] [강화석 획득!]\n", gold);
+	}
+	else sprintf(result, "\n [%d골드 획득!]\n", gold);
+
+	return result;
+}
+
+void CUser::LevelUp() {
+	int tmp;
+	SetLevel(GetLevel() + 1);
+	switch (GetLevel()) {
+	case 10:
+		skill[0] = true;
+		break;
+	case 20:
+		skill[1] = true;
+		break;
+	case 30:
+		skill[2] = true;
+		break;
+	case 40:
+		skill[3] = true;
+		break;
+	}
+
+	tmp = GetMaxHP();
+	SetMaxHP(tmp + GetLevel() * 50);
+	SetHP(GetMaxHP());
+
+	tmp = GetMaxMP();
+	SetMaxMP(tmp + GetLevel() * 50);
+	SetMP(GetMaxMP());
+
+	SetSTR(GetSTR() + 1);
+	SetDEX(GetDEX() + 1);
+	SetDEF(GetDEF() + 1);
+
+	mAccuracy++;
+	mExp = mExp - mTotalExp;
+	mPoint += 5;
+	mTotalExp += mTotalExp / 10;
+}
+
+void CUser::AutoRecovery() {
+	int tmp;
+	tmp = GetHP() + mAccuracy;
+	if (tmp > GetMaxHP())
+		SetHP(GetMaxHP());
+	else
+		SetHP(tmp);
+
+	tmp = GetMP() + mAccuracy;
+	if (tmp > GetMaxMP())
+		SetMP(GetMaxMP());
+	else
+		SetMP(tmp);
+}
+
+void CUser::LoseExp() {
+	int tmp = (mTotalExp / 10) * 3;
+	if (mExp < tmp) mExp = 0;
+	else mExp -= tmp;
 }
--- mudGame/user.h
+++ mudGame/user.h
@@ -3,18 +3,51 @@
 #include "monster.h"
 #pragma warning(disable:4996)
 
-class CUser :public CObject{
-	bool mb_normalAtk, mb_skillAtk;
-	int mWeaponATK, mPoint, mExp, mTotalExp, mGold;
-	unsigned char skill;
+class CUser :public CObject {
+	//무기 공격력, 정확도, 회복력, 포인트, 경험치, 총 경험치, 골드, 아이템
+	int mWeaponClass, mWeaponATK, mAccuracy, mResilience, mPoint, mExp, mTotalExp, mGold, mItem;
+	bool skill[4];
+	static int killCount;
 public:
 	CUser(string);
 
-	string Attack(CObject* pObject);
+	string Attack(CObject* pMonster);			//기본 공격
 
-	//void SkillAttack(CObject* pObject);
+	string SkillActivate(CObject* pMonster);	//스킬 활성화
 
+	bool AttackFail(CObject* pMonster);			//공격 성공 여부
+
+	//스킬 사용
+	string TripleAttack(CObject* pMonster, int mp);
+	string Recovery(int mp);
+	string TrueAttack(CObject* pMonster, int mp);
+	string AuraBlade(CObject* pMonster, int mp);
+
+	char* Loot(CObject* pMonster); //전리품-경험치, 골드, 아이템
+
+	//get
+	int GetWeaponClass() { return mWeaponClass; }
+	int GetWeapon() { return mWeaponATK; }
+	int GetAccuracy() { return mAccuracy; }
+	int GetResilience() { return mResilience; }
+	int GetPoint() { return mPoint; }
 	int GetExp() { return mExp; }
 	int GetTotalExp() { return mTotalExp; }
 	int GetGold() { return mGold; }
+	int GetItem() { return mItem; }
+	int GetKillCount() { return killCount; }
+	bool GetSkill(int i) { return skill[i]; }
+
+	//set
+	void SetKillCountZero() { killCount = 0; }
+	void InhanceWeapon() { mWeaponATK += 50; }
+	void WeaponUP() { mWeaponClass++; }
+	void SetAccuracy() { mAccuracy++; }
+	void SetResilience() { mResilience += 10; }
+	void SetPoint() { mPoint--; }
+	void SetItemGold(int num) { mItem -= num, mGold -= num * 100; }
+
+	void LevelUp();			//레벨업
+	void AutoRecovery();	//자동회복
+	void LoseExp();			//경험치 손실- 죽었을 경우, 중간 퇴장
 };
