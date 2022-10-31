e9a34ef6295fbe4050f20f2488cbe1b59e1ce990
gghtn <ghtn9898@gmail.com>
2022-10-31 23:11:32 +0900

    enum 값

---
+++ mudGame/key_pos.h
@@ -0,0 +1,43 @@
+#pragma once
+
+enum {
+	ENTER = 13,
+	ESC = 27,
+	SPACE = 32,
+	UP = 72,
+	LEFT = 75,
+	RIGHT = 77,
+	DOWN = 80,
+	A_KEY = 97,
+	S_KEY = 115,
+	D_KEY = 100,
+	F_KEY = 102
+};
+
+enum {
+	POS_BASIC = 17,
+	POS_GOBLIN = 20,
+	POS_ORC = 23,
+	POS_DEMON = 26,
+	POS_MAIN = 29,
+	POS_NORMAL_ATTACK = 57,
+	POS_SKILL_ATTACK = 87
+};
+
+// InhanceMenu yÁÂÇ¥
+enum {
+	POS_ITEMGOLD = 17,
+	POS_WEAPONATK = 21,
+	POS_INHANCE = 26
+};
+
+// InhanceHero yÁÂÇ¥
+enum {
+	POS_HP = 15,
+	POS_MP = 17,
+	POS_STR = 19,
+	POS_DEX = 21,
+	POS_DEF = 23,
+	POS_ACC = 25,
+	POS_RES = 27
+};
