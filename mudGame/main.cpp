5867af637d3d01d318e8a71e8babdb2e6a0ddb79
gghtn <ghtn9898@gmail.com>
2022-10-31 23:01:11 +0900

    main

--- mudGame/main.cpp
+++ mudGame/main.cpp
@@ -1,23 +1,13 @@
 #include <iostream>
 #include "main_space.h"
-#include "dungeon.h"
 using namespace std;
-
-
 
 int main() {
 	srand((unsigned)time(NULL));
 	SetConsoleSize(125, 50);
-	//CMainSpace begin;
-	//begin.MainStart();
-	
-	CObject* pUser = new CUser("test");
-	CDungeon scarecrowDungeon(pUser);
-	scarecrowDungeon.DungeonStart();
 
-	/*char sdf[123] = "Sdfa";
-	string str = "\n  sdffff";
-	strcat(sdf, str.c_str());
-	cout << sdf;*/
+	CMainSpace begin;
+	begin.MainStart();
+
 	return 0;
 }
